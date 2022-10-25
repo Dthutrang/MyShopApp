fecef58882af50490dcd138818cdaa5e5fffcf73
Dthutrang <daothutrang15082001@gmail.com>
2022-10-25 15:56:51 +0700

    commit 2

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,4 @@
-import 'package:flutter/material.dart';
-import 'ui/screens.dart';
-import 'package:provider/provider.dart';
 
-void main() {
-  runApp(const MyApp());
-}
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
